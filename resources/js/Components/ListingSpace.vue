241ecea649a909e21705c4cc52b227f723ddbb8b
ErnestasDapkus <ernestasdapkus1@gmail.com>
2023-03-29 22:08:54 +0300

    Grid element update

---
+++ resources/js/Components/ListingSpace.vue
@@ -0,0 +1,14 @@
+<template>
+  <diV>
+    <span class="font-bold">{{ listing.beds }}</span> bds <span class="text-gray-400">| </span>
+    <span class="font-bold">{{ listing.baths }}</span> ba <span class="text-gray-400">| </span>
+    <span class="font-bold">{{ listing.area }}</span> m²
+  </diV>
+</template>
+
+<script setup>
+
+defineProps({
+  listing: Object,
+})
+</script>
